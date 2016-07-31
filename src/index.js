b6a5f60de7425fa0a3f38c48b605312aed8fea79
Oleg Slobodskoi <oleg008@gmail.com>
2016-07-31 04:50:04 +0200

    implement $ref, update deps
    cssinjs/jss#214

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-const regExp = /&/g
+const warn = console.warn.bind(console) // eslint-disable-line no-console
+const parentRegExp = /&/g
+const refRegExp = /\$(\w+)/g
+
+/**
+ * Get a function to be used for $ref replacement.
+ */
+function getReplaceRef(sheet) {
+  return (match, name) => {
+    const rule = sheet.getRule(name)
+    if (rule) return rule.selector
+    warn(`JSS: could not find the referenced rule ${name}.`)
+    return name
+  }
+}
 
 /**
  * Convert nested rules to separate, remove them from original styles.
@@ -12,6 +26,7 @@ export default function jssNested() {
     const {sheet, jss, parent} = rule.options
     let container = sheet || jss
     let options
+    let replaceRef
 
     if (parent && parent.type === 'conditional') {
       container = parent
@@ -20,7 +35,16 @@ export default function jssNested() {
     for (const prop in rule.style) {
       if (prop[0] === '&') {
         if (!options) options = {...rule.options, named: false}
-        const name = prop.replace(regExp, rule.selector)
+        // Lazily create the ref replacer function just once for all nested rules within
+        // the sheet.
+        if (!replaceRef) replaceRef = getReplaceRef(container)
+
+        const name = prop
+          // Replace all & by the parent selector.
+          .replace(parentRegExp, rule.selector)
+          // Replace all $ref.
+          .replace(refRegExp, replaceRef)
+
         container.createRule(name, rule.style[prop], options)
         delete rule.style[prop]
       }
